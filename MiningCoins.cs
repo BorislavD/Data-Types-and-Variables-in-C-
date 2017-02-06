782236fde985ab5024468555b511e729a7d44178
Borislav Dermendzhiev <lenamarltd@gmail.com>
2017-02-06 13:32:21 +0200

    Added Mining Coins and Tetris

---
+++ MiningCoins.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiningCoins
+{
+    class MiningCoins
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string result = String.Empty;
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                List<int> list1 = new List<int>();
+                List<int> list2 = new List<int>();
+                int threeDigitNumber = int.Parse(Console.ReadLine());
+                
+                while (threeDigitNumber > 0)
+                {
+                    int lastDigit = threeDigitNumber % 10;
+                    list1.Insert(0, lastDigit);
+                    threeDigitNumber = threeDigitNumber / 10;
+                }
+
+                list2.Add(list1[0]);
+                list2.Add(list1[2]);
+
+                int num = 10 * list2[0] + list2[1];
+
+                if (i % 2 != 0)
+                {
+                    num = num + list1[1];
+                }
+                else
+                {
+                    num = num - list1[1];
+                }
+
+                if ((num >= 65 && num <= 90) || (num >= 97 && num <= 122))
+                {
+                    result = result + (char)num;
+                }
+
+                sum += list1[0] + list1[1] + list1[2];
+            }
+
+            float valueResult = (float)sum / n;
+            Console.WriteLine("Message: {0}", result);
+            Console.WriteLine("Value: {0:F7}", valueResult);
+        }
+    }
+}
---
+++ Tetris.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris
+{
+    class Tetris
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            while (!input.Equals("exit"))
+            {
+                if (input.Equals("left"))
+                {
+                    PrintLeft(n);
+                }
+                else if (input.Equals("right"))
+                {
+                    PrintRight(n);
+                }
+                else if (input.Equals("down"))
+                {
+                    PrintDown(n);
+                }
+                else
+                {
+                    PrintUp(n);
+                }
+
+                input = Console.ReadLine();
+            }
+        }
+
+        private static void PrintLeft(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(new string('.', n));
+                Console.WriteLine(new string('*', n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', 2 * n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(new string('.', n));
+                Console.WriteLine(new string('*', n));
+            }
+        }
+
+        private static void PrintRight(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(new string('*', n));
+                Console.WriteLine(new string('.', n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', 2 * n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(new string('*', n));
+                Console.WriteLine(new string('.', n));
+            }
+        }
+
+        private static void PrintDown(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', 3 * n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(new string('.', n));
+                Console.Write(new string('*', n));
+                Console.WriteLine(new string('.', n));
+            }
+        }
+
+        private static void PrintUp(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(new string('.', n));
+                Console.Write(new string('*', n));
+                Console.WriteLine(new string('.', n));
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine(new string('*', 3 * n));
+            }
+        }
+    }
+}
