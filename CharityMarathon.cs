2cd291fa33fa5eaaade07d67d140e842712a8a7e
Borislav Dermendzhiev <lenamarltd@gmail.com>
2017-02-08 09:30:40 +0200

    Added Charity Marathon

---
+++ CharityMarathon.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharityMarathon
+{
+    class CharityMarathon
+    {
+        static void Main(string[] args)
+        {
+            int days = int.Parse(Console.ReadLine());
+            int runners = int.Parse(Console.ReadLine());
+            int laps = int.Parse(Console.ReadLine());
+            int length = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            decimal moneyPerKM = decimal.Parse(Console.ReadLine());
+
+            long runnersMax = days * capacity;
+            long participants = runnersMax - runners;
+            if (participants >= 0)
+            {
+                participants = runners;
+            }
+            else
+            {
+                participants = runnersMax;
+            }
+
+            ulong totalMeters = (ulong)(participants * laps * length);
+            decimal totalKilometers = totalMeters / 1000m;
+
+            decimal rezult = totalKilometers * moneyPerKM;
+            Console.WriteLine("Money raised: {0:f2}", rezult);
+        }
+    }
+}
