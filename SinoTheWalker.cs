f2f960aefe173c719d29e46d29d4eeabff2e2509
Borislav Dermendzhiev <lenamarltd@gmail.com>
2017-02-09 09:59:29 +0200

    Added Sino The Walker

---
+++ SinoTheWalker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SinoTheWalker
+{
+    using System.Data.SqlTypes;
+    using System.Globalization;
+    using System.Numerics;
+
+    class SinoTheWalker
+    {
+        static void Main(string[] args)
+        {
+            TimeSpan inputTime = TimeSpan.Parse(Console.ReadLine());
+            int steps = int.Parse(Console.ReadLine()) % 86400;
+            int timeInSecondsForEachStep = int.Parse(Console.ReadLine()) % 86400;
+            long secondsToAdd = (long)(steps * timeInSecondsForEachStep);
+            TimeSpan secondsToAddTransformed = TimeSpan.FromSeconds(secondsToAdd);
+            TimeSpan endTime = inputTime + secondsToAddTransformed;
+            string result = endTime.ToString("hh\\:mm\\:ss");
+            Console.WriteLine("Time Arrival: {0}", result);
+        }
+    }
+}
